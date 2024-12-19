80a15b1916ff2bec39c8e8db10c6ebfd1d66f9e3
Akshat Kumar <108181558+Akshatk871@users.noreply.github.com>
2024-12-20 01:38:52 +0530

    Setup Frontend and Backend

---
+++ BudgetBuddy/src/test/java/com/ADP/BudgetBuddy/BudgetBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ADP.BudgetBuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
